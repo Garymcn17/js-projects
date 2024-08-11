c345fd63fb367b74823c875ddc62e9f0e7fbe0a7
Gary Mc Nulty <garymcn17@gmail.com>
2024-08-11 18:26:49 +0100

    using diff getElementById etc

--- counter/script.js
+++ counter/script.js
@@ -1,9 +1,11 @@
-const incrementEl = document.querySelector(".increment");
+//const incrementEl = document.querySelector(".increment");
+const incrementEl = document.getElementById("increment");
+const incrementEl1 = document.getElementsByClassName("increment");
 const decrementEl = document.querySelector(".decrement");
 const resetEl = document.querySelector(".reset");
 let counterEl = document.querySelector(".counter-value");
 
-incrementEl.addEventListener("click", () => {
+incrementEl1[0].addEventListener("click", () => {
   const value = +counterEl.textContent;
 
   counterEl.textContent = value + 1;
